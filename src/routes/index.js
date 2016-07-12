6093b262d0bb0f5c7b7cebc1d8606d72cda3e4f1
mue <mue.miv@gmail.com>
2016-07-12 07:07:49 +0300

    add Access-Control-Allow-Methods 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
     app.use(function (request, response, next) {
         response.header("Access-Control-Allow-Origin", "*");
         response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        response.header["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        response.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
         next();
     });
 
